f3dc21418dcac76367cdf0e4adb9c83d58f379c7
Matias Baldanza <matiasbaldanza@gmail.com>
2023-06-30 12:21:32 -0300

    feat: responsive positioning for hero section image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,9 +35,17 @@ export default function Home() {
             <h3>Keep track of your snippets</h3>
             <p>Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.</p>
           </div>
-          <div className='flex flex-col items-center gap-8 md:flex-row md:gap-14'>
-            <Image src='/img/image-computer.png' width={500} height={500} alt='Computer illustration'/>
-            <div className='flex flex-col gap-8 md:text-left'>
+          <div className='flex flex-col items-center w-full gap-8 md:flex-row'>
+            <div className='w-full h-full md:relative md:basis-1/2 md:h-[32rem]'>
+              <Image
+                className='h-full md:object-cover md:absolute md:top-0 md:right-0 md:overflow-x-visible md:-translate-x-1/2 xl:-translate-x-12'
+                src='/img/image-computer.png'
+                width={752}
+                height={572}
+                alt='Computer illustration'
+              />
+            </div>
+            <div className='flex flex-col gap-8 md:text-left md:basis-2/3'>
                 <div className='flex flex-col gap-2'>
                   <h4>Quick Search</h4>
                   <p>Easily search your snippets by content, category, web address, application, and more.</p>
@@ -148,4 +156,4 @@ export default function Home() {
       </footer>
     </div>
   )
-}
+} 
