4b839dbd2db656bad94c72d6f74076704d04cfad
Matias Baldanza <matiasbaldanza@gmail.com>
2023-06-30 17:59:21 -0300

    style: adjust typographic scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,10 @@ module.exports = {
     extend: {
       fontSize: {
           sm: '0.8rem',
-          base: '1rem',
-          xl: '1.25rem',
-          '2xl': '1.5rem', 
-          '3xl': '2rem',
+          base: '1.2rem',
+          xl: '1.5rem',
+          '2xl': '2rem', 
+          '3xl': '2.25rem',
           '4xl': '2.5rem',
           '5xl': '3rem',
       },
