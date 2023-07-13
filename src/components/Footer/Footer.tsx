c70036eb196774640f060ad684dedbd85f01a303
Matias Baldanza <matiasbaldanza@gmail.com>
2023-07-12 22:46:05 -0300

    fix: adjust max-width for Footer content to match main page container

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import SocialLinks from '../SocialLinks'
 function Footer () {
   return (
     <footer className='py-16 m-auto mt-32 bg-background min-h-7xl'>
-      <div className='flex flex-col items-center gap-12 px-8 m-auto md:flex-row md:justify-between md:max-w-screen-2xl'>
+      <div className='flex flex-col items-center gap-12 px-8 m-auto md:flex-row md:justify-between md:max-w-screen-xl'>
         <Image src='/img/logo.svg' height={72} width={72} alt='Clipboard logo' />
         <NavMenu />
         <SocialLinks />
